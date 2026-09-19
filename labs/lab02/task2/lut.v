31d6f418bd506095f678300238596ca4454ce4a4
sahil-khose <f20251449@pilani.bits-pilani.ac.in>
2026-09-19 12:19:36 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -6,23 +6,30 @@
 
 module lut #(
   parameter WIDTH = 8,
-  parameter DEPTH = 4
+      parameter DEPTH = 4
 ) (
-  input      [$clog2(DEPTH)-1:0] sel,
+  input        [$clog2(DEPTH)-1:0] sel,
   output reg [WIDTH-1:0]         dout
 );
 
-  reg [WIDTH-1:0] mem [0:DEPTH-1];
+    reg [WIDTH-1:0] mem [0:DEPTH-1];
 
   integer i;
 
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
   // contents should be set up. (See the lab manual for why.)
+  initial begin
+      for (i = 0; i < DEPTH; i = i + 1) begin
+        mem[i] = i * i;
+    end
+  end
   
 
   // TODO: make dout continuously reflect mem[sel]. This is a combinational
   // read -- pick the right procedural block and sensitivity list.
-
+   always @(*) begin
+      dout = mem[sel];
+  end
 
 endmodule
